a91bf47a1ab984941a6689385cfbae841234ea7b
krishnapriyakr <krishnapriyakrpriya@gmail.com>
2024-11-08 17:11:05 +0530

    homerentals_frontend

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -69,79 +69,7 @@ function Wishlist() {
     }
   }
   
-  //booking from wishlist 
-  // const [shows, setShows] = useState(false);
-  // const handleClose0 = () => setShows(false);
-  // const handleShow0 = () => setShows(true);
 
-  // const [bookingDetails, setBookingDetails] = useState({
-  //    date:"",days:"",total:""
-  // })
-  // const [totalprice, setTotalprice] = useState()
-
-  
-  
-  // const BookingFromWishlist =async () => {
-  //   handleClose()
-  //   // e.preventDefault()
-  // const { id, date, days } = bookingDetails;
-  //     const price=setselectProperty.property_id.price
-  //     setTotalprice(price*bookingDetails.days)
-  // if (!date || !days) {
-  //   alert("Please fill empty fields")
-  // }
-  // else {
-    
-  //   const reqBody = new FormData()
-  //   reqBody.append('date', date)
-  //   reqBody.append('days', days)
-  //   // reqBody.append('total',total)
-     
-  //   if (token) {
-  //     const reqHeader = {
-  //       "Content-Type": "application/json",
-  //       "Authorization": `Bearer ${token}`
-  //     };
-  //     try {
-  //       const result = await BookFromWishlistAPI(id,reqBody, reqHeader)
-  //       if (result.status === 200) {
-  //         console.log(result.data);
-  //         alert("Booking Successfull")
-  //         setBookingDetails({
-  //           date:"",days:"",total:""
-  //         })
-  //         handleClose0(true)
-  //       }
-  //       else {
-  //         console.log(result);
-  //         console.log(result.response.data);
-  //         alert("The property is not available for the chosen days. Please change the date and try again")
-  //         handleClose0(true)
-  //       }
-      
-        
-  //     }
-  //     catch(error) {
-  //       console.error("Booking Api call failled:",error);
-        
-  //     }
-      
-  //   }
-    
-  // }
-  // }
-  
-  // const[token,setToken]=useState("")
-  // useEffect(() => {
-  //   if (sessionStorage.getItem("token"))
-  //   {
-  //       setToken(sessionStorage.getItem("token"))
-  //   }
-  //   else {
-  //       setToken("")
-  //   }
-  //   // getAllProperty()
-  // }, [])
 
   return (
       <>
@@ -248,58 +176,9 @@ function Wishlist() {
           
           </div>
           
-           {/* <div className="button d-flex justify-content-start alighn-items-start" >
-                    <Button onClick={handleShow0} >Book now</Button>
-                  </div> */}
-             {/* < div className="booking">
-        <Modal show={shows} onHide={handleClose0}  centered >
-        <Modal.Header closeButton>
-          <Modal.Title> </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Select start date </Form.Label>
-              <Form.Control
-                type="date"
-                placeholder=""
-                  autoFocus
-                  value={bookingDetails.date} onChange={e=>setBookingDetails({...bookingDetails,date:e.target.value})}
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>How long do you want to stay </Form.Label>
-              <Form.Control type='number' placeholder="in days" value={bookingDetails.days} onChange={e=>setBookingDetails({...bookingDetails,days:e.target.value})} />
-            </Form.Group>
-              </Form>
-                      
-            <div className="price">
-
-                  total price: <b> $ {totalprice}  </b>
-                  
-                   </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose0}>
-            Close
-          </Button>
-                <Button variant="primary"  onClick={BookingFromWishlist}  >
-                {/**/}
-            {/* Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal> 
-          </div>   */}
-            
-          
-          
         </div>
         
        
-
 
           {/* <center> <img height={'350px'} className='mt-5'  src="https://i.pinimg.com/originals/29/5a/50/295a50b21af59b87804f2fa5a3d2fe4a.gif" alt="" /> </center>
             <h3 className='text-center text-danger'> <b> Wishlist is Empty!! </b></h3> <br />
--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,1 +1,4 @@
 export const BASE_URL = 'https://homerentals-backend.onrender.com'
+
+// https://homerentals-backend.onrender.com
+// http://localhost:4000
